6b7045b2daa3a68e1a8675448d28e50797dd0d04
Pedro Pereira <palex@cc.isel.ipl.pt>
2021-05-04 18:35:47 +0100

    Aula 17 - 4 Maio
    List<T>
    map; filter; forEach;

--- Aula16/src/main/kotlin/position.kt
+++ Aula16/src/main/kotlin/position.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sqrt
+
 val VELOCITY_LIMITS = -10..10
 
 data class Position(val x:Int, val y:Int)
@@ -6,6 +8,11 @@ data class Motion(val dx:Int, val dy:Int)
 
 //fun plus( p:Position, m:Motion) = Position(p.x+m.dx, p.y+m.dy)
 operator fun Position.plus(m:Motion) = Position(x + m.dx, y + m.dy)
+
+
+fun distance(a :Position, b :Position) = sqrt( (square(a.x-b.x) + square(a.y-b.y)).toFloat() )
+
+fun square( n :Int ) = n * n
 
 fun randomMotion() = Motion( VELOCITY_LIMITS.random() , VELOCITY_LIMITS.random() )
 
--- Aula16/src/main/kotlin/theBall.kt
+++ Aula16/src/main/kotlin/theBall.kt
@@ -9,9 +9,9 @@ private fun Motion.toColor() = when {
     else -> BLACK
 }
 
-private fun Canvas.drawBall(b: Ball?) {
+private fun Canvas.drawBalls(bs: List<Ball>) {
     erase()
-    if (b!=null) {
+    bs.forEach { b ->
         val color = b.motion.toColor()
         drawCircle(b.position.x, b.position.y, b.radius, color)
     }
@@ -21,17 +21,21 @@ fun main() {
     onStart {
         val cv = Canvas(300,600)
         var balls = listOf<Ball>()
-        cv.drawBall(ball)
+        cv.drawBalls(balls)
         cv.onTimeProgress(20) {
             balls = balls.map { it.move(cv.width,cv.height) }
-            //TODO: convert function drawBall() in drawBalls()
-            cv.drawBall(ball)
+            cv.drawBalls(balls)
         }
         cv.onKeyPressed {
-            if (it.char=='b')
-                balls = balls + Ball(Position(0,0),randomMotion(),RADIUS)
+            if (it.char=='b') {
+                balls = balls + Ball(Position(0, 0), randomMotion(), RADIUS)
+            }
         }
-        //cv.onMouseDown { ball = ball?.copy(position= Position(it.x,it.y)) }
+        cv.onMouseDown {
+            val mp = Position(it.x,it.y)
+            val clicked = balls.filter{ b -> distance(b.position,mp) <= b.radius }
+            balls = balls - clicked
+        }
     }
     onFinish {  }
 }
